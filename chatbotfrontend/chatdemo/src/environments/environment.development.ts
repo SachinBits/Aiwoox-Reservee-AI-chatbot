19ddf9111e192ee4430177fe4034141011e66cb8
SachinBits <f20230208@dubai.bits-pilani.ac.in>
2025-08-06 13:17:38 +0800

    Update environment.development.ts

--- chatbotfrontend/chatdemo/src/environments/environment.development.ts
+++ chatbotfrontend/chatdemo/src/environments/environment.development.ts
@@ -1,4 +1,7 @@
 export const environment = {
     production: false,
-    
+    // SUPABASE_URL: 'https://dhbbaorrjrwuagxqqmij.supabase.co',
+    // SUPABASE_ANON_KEY: 'LCJpYXQiOjE3MzU0OTIzNTksImV4cCI6MjA1MTA2ODM1OX0.6yGhD1kUX0msvA-VT3KhYX4NlrQcfsjWXmBQTRAgzyo',
+    // azureSpeechKey: '',
+    // azureSpeechRegion: ''
 };
